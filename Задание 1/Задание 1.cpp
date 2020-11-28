e8906807031475f00a73bd0d4fe772158df5d697
Бабиков Дмитрий <dima.babikov.02@mail.ru>
2020-11-28 18:36:19 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Задание 1.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int n = 1;
+    char a[n] = { 'л' };
+    const int l = 15;
+    char b[l] = "Колледж ВятГУ";
+
+    cout << "Символ для поиска в строке - л\n";
+    cout << "Строка - Колледж ВятГУ\n";
+
+    int k = 0;
+    for (int i = 0; b[i]; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (b[i] == a[j])
+                k++;
+        }
+    }
+    if (k)
+        cout << "Количество найденных символов: " << k << endl;
+    else
+        cout << "Данного символа не найдено!" << endl;
+    cin.get();
+    return 0;
+}
---
+++ Задание 6/Задание 6.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    string s, so;
+    int n = 0, p = 0;
+    cout << "Введите строку: ";
+    cin >> s;
+    cout << "Введите подстроку: ";
+    cin >> so;
+    while ((p = s.find(so, p)) != s.npos) {
+        ++n;
+        ++p;
+    }
+    cout << "Подстрока " << so << " в строке " << s << " содержит " << n << " вхождений!" << endl;
+    return 0;
+}
